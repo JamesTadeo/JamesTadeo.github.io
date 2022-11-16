feb6c7c8c46639ce721fb57904c01f062579dd51
James Tadeo <jtadeo@gmail.com>
2022-11-16 09:17:21 -0500

    Functions review

--- coursera/module4-wip/js/functions.js
+++ coursera/module4-wip/js/functions.js
@@ -1,3 +1,6 @@
+//Check out https://jamestadeo.github.io/coursera/
+
+
 // https://www.coursera.org/learn/html-css-javascript-for-web-developers/lecture/4tOjk/lecture-41-part-1-defining-variables-function-and-scope
 
 //test hello - set up console
