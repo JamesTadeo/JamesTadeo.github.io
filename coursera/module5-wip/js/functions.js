5651c6fe2c6a60942ec9a2bc20a2ab8ba9d405ce
James Tadeo <jtadeo@gmail.com>
2022-11-20 06:41:26 -0500

    Resolved functions.js file merge issue. Couldn't rebase it.

---
+++ coursera/module5-wip/js/functions.js
@@ -0,0 +1,12 @@
+/*
+Module 5 Solution - DC Random Specials Menu
+
+Here's the shareable link:
+https://www.coursera.org/learn/html-css-javascript-for-web-developers/peer/4p7AF/module-5-coding-assignment/review/XeNJUmhnEe2T5gqK_nBZmw
+
+https://www.coursera.org/learn/html-css-javascript-for-web-developers/peer/4p7AF/module-5-coding-assignment/review/XeNJUmhnEe2T5gqK_nBZmw
+
+Here's the main link:
+https://jamestadeo.github.io/coursera/module5-solution/
+
+*/
