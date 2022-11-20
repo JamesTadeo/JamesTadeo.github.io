42c8cc7feaaba51a5ef775fbda6169a4ce7d9044
James Tadeo <jtadeo@gmail.com>
2022-11-19 19:24:24 -0500

    Updated js

--- coursera/module5-wip/js/functions.js
+++ coursera/module5-wip/js/functions.js
@@ -5,6 +5,8 @@ Here's the shareable link:
 https://www.coursera.org/learn/html-css-javascript-for-web-developers/peer/4p7AF/module-5-coding-assignment/review/XeNJUmhnEe2T5gqK_nBZmw
 
 
+https://www.coursera.org/learn/html-css-javascript-for-web-developers/peer/4p7AF/module-5-coding-assignment/review/XeNJUmhnEe2T5gqK_nBZmw
+
 Here's the main link:
 https://jamestadeo.github.io/coursera/module5-solution/
 
